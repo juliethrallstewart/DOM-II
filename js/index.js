95fc0ffdaf2ec25330809da81b047399a7058eec
Julie Thrall <juliethrall10@gmail.com>
2019-06-18 22:34:36 -0700

    add UI

--- js/index.js
+++ js/index.js
@@ -10,13 +10,14 @@
 
 const nav = document.querySelector('nav');
 nav.addEventListener('click', event => {
-    console.log("I was clicked");
+    //console.log("I was clicked");
     event.preventDefault;
 })
 
 // nav links using 'click'
 
 const navLinks = document.querySelectorAll('nav a');
+
 //console.log(navLinks)
 
 
@@ -34,22 +35,11 @@ function randomColor() {
 }
 
 
-//When mousing over images become grayscale & revert using 'mouseover' & 'mouseout'
+//When mousing over images become grayscale & revert to saturate using 'mouseover' & 'mouseout'
 const images = document.querySelectorAll('img');
-console.log(images)
+//console.log(images)
 
 images.forEach(item => item.addEventListener('mouseover', grayScaleImg));
-
-// const image1 = document.getElementsByTagName('img')[0];
-// const image2 = document.getElementsByTagName('img')[1];
-// const image3 = document.getElementsByTagName('img')[2];
-// const image4 = document.getElementsByTagName('img')[3];
-
-// image1.addEventListener('mouseover', grayScaleImg);
-// image2.addEventListener('mouseover', grayScaleImg);
-// image3.addEventListener('mouseover', grayScaleImg);
-// image4.addEventListener('mouseover', grayScaleImg);
-
 
 function grayScaleImg() {
     if (event.target.style.filter !== "grayscale(1)") {
@@ -60,23 +50,72 @@ function grayScaleImg() {
 }
 
 function colorImg() {
-    event.target.style.filter = "none";
+    event.target.style.filter = "saturate(100%)";
 }
 
-// text background color using 'dblclick'
+// REMOVED text background color using 'dblclick'
 
-let text0 = document.getElementsByClassName('text-content')[0];
-let text1 = document.getElementsByClassName('text-content')[1];
+// let text0 = document.getElementsByClassName('text-content')[0];
+// let text1 = document.getElementsByClassName('text-content')[1];
 
-text0.addEventListener('dblclick', background);
-text1.addEventListener('dblclick', background);
+// text0.addEventListener('dblclick', background);
+// text1.addEventListener('dblclick', background);
 
+// double-clicking anywhere on the page turns the background-color a new color, double clicking again reverts it
+const body = document.querySelector('html');
+body.addEventListener('dblclick', background);
 
 function background() {
-    event.target.style.backgroundColor = "orange";
+    if (event.target.style.backgroundColor !== "orange") {
+        event.target.style.backgroundColor = "orange";
+    } else if (event.target.style.backgroundColor === "orange") {
+        event.target.style.backgroundColor = "";
+    }
 }
 
-//removed styling
+// use keys to change the color of the h2 headers using 'keydown' on the window
+// e.code returns the keyCode associated with the keydown
+
+window.addEventListener('keydown', keyColor)
+
+function keyColor(e) {
+    const h2 = document.querySelectorAll('h2');
+    switch (e.code) {
+        case "KeyA":
+            h2.forEach(item => item.style.color = 'orange');
+            break;
+        case "KeyS":
+            h2.forEach(item => item.style.color = 'teal');
+            break;
+        case "KeyD":
+            h2.forEach(item => item.style.color = 'red');
+            break;
+        case "KeyF":
+            h2.forEach(item => item.style.color = 'purple');
+            break;
+    }
+}
+
+// when scrolling console.log 'Wheeeeeeeee!' using 'scroll'
+
+window.addEventListener('scroll', event => console.log("Wheeeeeeeeee!"))
+
+//  when resizing the window display height & width in the console *ONLY*
+const heightOutput = document.getElementById("height")
+const widthOutput = document.getElementById("width")
+
+const windowSizingText = document.querySelectorAll(".windowSizing")
+windowSizingText.forEach(item => item.style.display = "none");
+
+
+window.addEventListener('resize', event => {
+    heightOutput.innerHTML = window.innerHeight;
+    widthOutput.innerHTML = window.innerWidth;
+    console.log(heightOutput, widthOutput)
+})
+
+
+// removed
 //navLinks.forEach(item => item.addEventListener('focus', highlight)
 // function highlight() {
 //     if (event.target.style.backgroundColor !== 'lightblue') {
